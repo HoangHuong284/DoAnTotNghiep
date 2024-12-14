2e0a4d8172f9d478a236279f56b59d273b44dc8f
HoangHuong284 <hoanghuong28042002@gmail.com>
2024-12-14 19:42:11 +0700

    Change index

---
+++ src/main/java/com/web/dto/ProductSearch.java
@@ -0,0 +1,16 @@
+package com.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductSearch {
+
+    private List<Long> categoryIds = new ArrayList<>();
+    Double minPrice;
+    Double maxPrice;
+}
